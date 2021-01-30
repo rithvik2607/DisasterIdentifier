ece3f1bad5f3772784629844a3ee4e636365ef2f
Rithvik <rithvikayithapu@gmail.com>
2021-01-30 18:47:44 +0530

    Corrections to model and added pickle files

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import numpy as np
 import pandas as pd
 from sklearn import feature_extraction, linear_model
 
+file = 'disaster_prediction_model.pkl'
+rclf = pickle.load(open(file, 'rb'))
+transformFile = 'transform.pkl'
+cv = pickle.load(open(transformFile, 'rb'))
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -14,17 +18,17 @@ def Home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-  train_data = pd.read_csv('train.csv')
-  test_data = pd.read_csv('test.csv')
-  count_vectorizer = feature_extraction.text.CountVectorizer()
-  train_vector = count_vectorizer.fit_transform(train_data['text'])
-  rclf = linear_model.RidgeClassifier()
-  rclf.fit(train_vector, train_data['target'])
+  # train_data = pd.read_csv('train.csv')
+  # test_data = pd.read_csv('test.csv')
+  # count_vectorizer = feature_extraction.text.CountVectorizer()
+  # train_vector = count_vectorizer.fit_transform(train_data['text'])
+  # rclf = linear_model.RidgeClassifier()
+  # rclf.fit(train_vector, train_data['target'])
 
   if request.method == 'POST':
     text = request.form['tweet']
     data = [text]
-    vector =  count_vectorizer.transform(data).toarray()
+    vector =  cv.transform(data).toarray()
     prediction = rclf.predict(vector)
     if prediction == 0:
       return render_template('index.html', prediction_text='Nothing to worry about')
